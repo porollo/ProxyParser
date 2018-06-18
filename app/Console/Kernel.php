5abb5f1d50e275270180d0efdfc95cb071334f13
Dmitriy S. Porollo <d.porollo@gmail.com>
2018-06-19 00:21:56 +0300

    Update 2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\RefreshProxyHosts;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,9 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\PingProxyHosts::class,
+        \App\Console\Commands\RefreshProxyHosts::class,
+
     ];
 
     /**
@@ -24,8 +27,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command(PingProxyHosts::class, ['--force'])->everyMinute();;
+        $schedule->command(RefreshProxyHosts::class, ['--force'])->everyMinute();;
     }
 
     /**
